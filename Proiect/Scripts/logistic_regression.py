ce311430b0c64cc1863f9ba584832f133dbec2d8
Puscas Mihai <puscas.mihai2064@gmail.com>
2026-02-03 02:10:41 +0200

    Data Clean

--- Proiect/Scripts/logistic_regression.py
+++ Proiect/Scripts/logistic_regression.py
@@ -5,3 +5,16 @@ from sklearn.model_selection import train_test_split
 import os
 
 dataframe = pd.read_csv(os.path.join('Data', 'train.csv'), encoding='latin-1')
+
+dataframe.drop(['PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
+
+dataframe['Sex'] = dataframe['Sex'].map({'female': 0, 'male': 1})
+
+dataframe['Age'] = dataframe['Age'].fillna(dataframe['Age'].median())
+
+dataframe['Has_Cabin'] = dataframe['Cabin'].notnull()
+dataframe['Has_Cabin'] = dataframe['Has_Cabin'].astype(int)
+
+dataframe['Embarked'] = dataframe['Embarked'].map({'S': 0, 'C': 1, 'Q': 2})
+
+dataframe['Fare'] = dataframe['Fare'].fillna(dataframe['Fare'].median())
